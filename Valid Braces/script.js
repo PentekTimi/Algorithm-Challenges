fc27302388d35f7ad2ad6591145b95e9aa248f13
PentekTimi <pentektimea98@gmail.com>
2024-02-02 00:30:34 +0000

    Add valid braces challenge

---
+++ Valid Braces/script.js
@@ -0,0 +1,10 @@
+function validBraces(braces) {
+  while (
+    braces.includes("()") ||
+    braces.includes("[]") ||
+    braces.includes("{}")
+  ) {
+    braces = braces.replace("()", "").replace("[]", "").replace("{}", "");
+  }
+  return braces.length === 0 ? true : false;
+}
