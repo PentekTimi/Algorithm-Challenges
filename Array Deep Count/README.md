541f2afd77f32f3a594c70597e7596e6c88ed25c
PentekTimi <pentektimea98@gmail.com>
2024-05-12 15:50:42 +0100

    Add array deep count challenge

---
+++ Array Deep Count/README.md
@@ -0,0 +1,9 @@
+You are given an array. Complete the function that returns the number of ALL elements within an array, including any nested arrays.<br>
+Examples<br>
+
+[] --> 0<br>
+[1, 2, 3] --> 3<br>
+["x", "y", ["z"]] --> 4<br>
+[1, 2, [3, 4, [5]]] --> 7<br>
+
+The input will always be an array.<br>
---
+++ Array Deep Count/script.js
@@ -0,0 +1,18 @@
+function deepCount(a) {
+  return a.reduce(
+    (accumulator, currentValue) =>
+      (accumulator += Array.isArray(currentValue)
+        ? deepCount(currentValue)
+        : 0),
+    a.length
+  );
+}
+
+//or
+
+function deepCount(a) {
+  let count = a.length;
+  for (let i = 0; i < a.length; i++)
+    if (Array.isArray(a[i])) count += deepCount(a[i]);
+  return count;
+}
