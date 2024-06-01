08e4bcb42f7b644e258454dc1b5cb35b425bd3b1
PentekTimi <pentektimea98@gmail.com>
2024-06-01 12:38:55 +0100

    Add sort the odd challenge

---
+++ Sort the odd/script.js
@@ -0,0 +1,29 @@
+function sortArray(array) {
+  const odds = array.filter((el) => el % 2).sort((a, b) => a - b);
+  return array.map((el) => (el % 2 ? odds.shift() : el));
+}
+
+// or
+
+function sortArray(array) {
+  var odds = [];
+  //loop, if it's odd, push to odds array
+  for (var i = 0; i < array.length; ++i) {
+    if (array[i] % 2 !== 0) {
+      odds.push(array[i]);
+    }
+  }
+  //sort odds from smallest to largest
+  odds.sort(function (a, b) {
+    return a - b;
+  });
+
+  //loop through array, replace any odd values with sorted odd values
+  for (var j = 0; j < array.length; ++j) {
+    if (array[j] % 2 !== 0) {
+      array[j] = odds.shift();
+    }
+  }
+
+  return array;
+}
