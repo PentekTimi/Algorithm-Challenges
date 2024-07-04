e54203c3dc2681e3c8640b470989d05de5688520
PentekTimi <pentektimea98@gmail.com>
2024-07-04 21:29:34 +0100

    Add array reduce transformation challenge

---
+++ Array Reduce Transformation/script.js
@@ -0,0 +1,10 @@
+var reduce = function (nums, fn, init) {
+  let result = init;
+  if (nums.length == 0) return init;
+
+  for (let i = 0; i < nums.length; i++) {
+    result = fn(result, nums[i]);
+  }
+
+  return result;
+};
