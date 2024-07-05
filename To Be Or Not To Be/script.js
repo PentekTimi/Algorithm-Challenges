304baedd4367456ea30cf667bf5d1d66c22539fd
PentekTimi <pentektimea98@gmail.com>
2024-07-05 22:33:06 +0100

    Add to be or not to be challenge

---
+++ To Be Or Not To Be/script.js
@@ -0,0 +1,18 @@
+var expect = function (val) {
+  return {
+    toBe(value) {
+      if (value == val) {
+        return true;
+      } else {
+        throw new Error("Not Equal");
+      }
+    },
+    notToBe(value) {
+      if (value !== val) {
+        return true;
+      } else {
+        throw new Error("Equal");
+      }
+    },
+  };
+};
