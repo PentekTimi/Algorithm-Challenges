ea99182fc950b8d5e6c5e05577aafb97410a3c84
PentekTimi <pentektimea98@gmail.com>
2024-09-22 12:20:06 +0100

    Add array partition challenge

---
+++ Array Partition/script.js
@@ -0,0 +1,9 @@
+var arrayPairSum = function (nums) {
+  let result = 0;
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length; i += 2) {
+    result += nums[i];
+  }
+  return result;
+};
