0c9c263f3a20b6c1f1a1ee474da1dd9c5c42d513
PentekTimi <pentektimea98@gmail.com>
2024-10-11 08:34:34 +0100

    Add faulty keyboard challenge

---
+++ Faulty Keyboard/script.js
@@ -0,0 +1,13 @@
+var finalString = function (s) {
+  let result = "";
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "i") {
+      result = result.split("").reverse().join("");
+    } else {
+      result += s[i];
+    }
+  }
+
+  return result;
+};
