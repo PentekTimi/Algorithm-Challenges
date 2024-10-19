750740bba18b2bf02d5ff19fe5c891edb185fed1
PentekTimi <pentektimea98@gmail.com>
2024-10-19 11:06:41 +0100

    Add repeated substring pattern challenge

---
+++ Repeated Substring Pattern/script.js
@@ -0,0 +1,11 @@
+var repeatedSubstringPattern = function (s) {
+  let len = s.length;
+
+  for (let i = 1; i <= Math.floor(len / 2); i++) {
+    if (len % i === 0 && s.slice(0, i).repeat(len / i) === s) {
+      return true;
+    }
+  }
+
+  return false;
+};
